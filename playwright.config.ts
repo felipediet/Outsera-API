7c0029be713a8fc73320c923979e5cf7b14915a4
Felipediet <fefola@gmail.com>
2025-06-16 20:41:02 -0300

    Atualizado config de env e reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,13 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+
+//Rotina que define qual o arquivo de configuração de ambiente será utilizado
+//Se a variável de ambiente ENVIRONMENT não for informada, será utilizado o arquivo .env
+const envfile = process.env.ENVIRONMENT ? `.env.${process.env.ENVIRONMENT}` : '.env';
+dotenv.config({ path: path.resolve(__dirname,'env', envfile) });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -22,7 +26,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html"], ["junit", { outputFile: "junit.xml" }]],
+  reporter: [
+    ["html"],
+    ["junit", { outputFile: "junit.xml" }],
+    ['@estruyf/github-actions-reporter']
+    ],
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -37,17 +46,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    }
 
     /* Test against mobile viewports. */
     // {
